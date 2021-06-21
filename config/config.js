bbd5285bd7a4719a5061f61bb2c4a30cd2b2b7d1
bruno-prata <email.prata@gmail.com>
2021-06-21 00:34:52 -0300

    Correção AWS DB

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
+  development: {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   dialect: "mysql",
+  }
+
 };
